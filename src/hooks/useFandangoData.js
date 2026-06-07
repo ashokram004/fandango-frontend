e5d860b268a58f1b4c22baaa9674296e495bfc65
Ashok Ram <77197519+ashokram004@users.noreply.github.com>
2026-06-07 14:17:47 +0530

    Update SHOW_DATE to '2026-06-07'

--- src/hooks/useFandangoData.js
+++ src/hooks/useFandangoData.js
@@ -3,7 +3,7 @@ import { ref, onValue } from 'firebase/database';
 import { database } from '../firebaseConfig';
 
 const MOVIE_SLUG = 'peddi-2026';
-const SHOW_DATE = '2026-06-06';
+const SHOW_DATE = '2026-06-07';
 
 const formatUtcToIst = (value) => {
   try {
